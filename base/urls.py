650a0c14d6da9b6f74039b3ba976c14907471871
Parth <mittalparth22@gmail.com>
2021-05-17 22:20:13 +0530

    Added individual club view

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from base import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"), 
+    path("clubview/<int:pk>", views.clubview, name="clubview"), 
 ]
--- base/views.py
+++ base/views.py
@@ -5,3 +5,9 @@ def index(request):
     clubs = Club.objects.all()
     context = {'clubs':clubs}
     return render(request, 'index.html',context)
+
+def clubview(request, pk):
+    club = Club.objects.filter(id = pk)
+    context = {'club': club[0]}
+
+    return render(request, 'clubview.html', context)
