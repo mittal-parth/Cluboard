e955d1717932cc5648eb7f7330d12b7c1c8ee1d8
Parth <mittalparth22@gmail.com>
2021-05-19 23:30:31 +0530

    Added models and some restructuring, updated to Bootstrap 5

--- base/admin.py
+++ base/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from base.models import Club
+from base.models import Club, Info, Request, Item
 # Register your models here.
 admin.site.register(Club)
+admin.site.register(Info)
+admin.site.register(Item)
+admin.site.register(Request)
--- base/models.py
+++ base/models.py
@@ -3,10 +3,30 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Club(models.Model):
-    #Many Clubs have one User
+    #Many Clubs have many Users
     users = models.ManyToManyField(User, blank=True, null=True)
     club_name =  models.CharField(max_length = 100)
     logo = models.ImageField(upload_to="images/", default = "")
 
     def __str__(self):
         return self.club_name
+
+class Info(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    designation = models.CharField(max_length=100, default="Member")
+    roll_no = models.CharField(max_length=100)
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    qty = models.IntegerField()
+    club = models.ManyToManyField(Club, blank=True, null = True)
+    image = models.ImageField(upload_to="images/", default = "")  
+
+class Request(models.Model):
+    STATUS = (('Pending', 'Pending'), ('Approved', 'Approved'), ('Rejected', 'Rejected'))
+    requested_by = models.ForeignKey(User, null=True, on_delete = models.SET_NULL)
+    item = models.ForeignKey(Item, null=True, on_delete=models.SET_NULL)
+    status = models.CharField(max_length=100, null=True, choices=STATUS)
+    date_created = models.DateField(auto_now_add=True)
+    
+
