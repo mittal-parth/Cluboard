82438cb937d185ffbae68d6c8af86d476fcbf343
Parth <mittalparth22@gmail.com>
2021-05-31 22:43:07 +0530

    Reset Password functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from accounts import views
 
@@ -7,4 +8,10 @@ urlpatterns = [
     path("login", views.loginPage, name= "login"),
     path("profile/<int:pk>/<int:user_id>", views.profile, name="profile"),
     path("logout", views.logoutPage, name= "logout"),
+
+    #They are url names and view functions are provided by default by Django
+    path("password_reset/", auth_views.PasswordResetView.as_view(template_name = 'password_reset.html'), name="password_reset"), 
+    path("password_reset_done/", auth_views.PasswordResetDoneView.as_view(template_name = 'password_reset_done.html'), name="password_reset_done"), 
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name = 'password_reset_confirm.html'), name="password_reset_confirm"), 
+    path("password_reset_complete/", auth_views.PasswordResetCompleteView.as_view(template_name = 'password_reset_complete.html'), name="password_reset_complete"), 
 ]
--- base/urls.py
+++ base/urls.py
@@ -5,16 +5,21 @@ from base import views
 urlpatterns = [
     path("", views.index, name="index"), 
     path("index_member/<int:pk>", views.index_member, name="index_member"), 
+
+    path("club_view/<int:pk>", views.club_view, name="club_view"), 
     path("club_add/", views.club_add, name="club_add"), 
+
     path("user_add/<int:pk>", views.user_add, name="user_add"), 
     path("user_delete/<int:user_id>", views.user_delete, name="user_delete"), 
+
     path("item_add/<int:pk>", views.item_add, name="item_add"), 
     path("item_update/<int:pk>/<int:item_id>", views.item_update, name="item_update"), 
     path("item_delete/<int:item_id>", views.item_delete, name="item_delete"), 
-    path("request_add/<int:pk>", views.request_add, name="request_add"), 
     path("items_view/<int:pk>", views.items_view, name="items_view"), 
-    path("club_view/<int:pk>", views.club_view, name="club_view"), 
+
+    path("request_add/<int:pk>", views.request_add, name="request_add"),   
     path("request_approve/<int:request_id>", views.request_approve, name="request_approve"), 
     path("request_reject/<int:request_id>", views.request_reject, name="request_reject"), 
-    path("error_page/", views.error_page, name="error_page"), 
+
+    path("error_page/", views.error_page, name="error_page"),    
 ]
