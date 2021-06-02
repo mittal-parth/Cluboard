d745328291e27136d52d18aa7b50b8f10f31858e
Parth <mittalparth22@gmail.com>
2021-06-02 10:35:56 +0530

    Changed settings.py

--- Inventory_Management/settings.py
+++ Inventory_Management/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n%soswvt@-)o2@i_q5q%6=gkh)tkl)y6o*_1-s-b9n#+fw5(@y'
+SECRET_KEY = os.environ.get('SECRET_KEY_InvManage')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
