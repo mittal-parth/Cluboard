a278ef84e1859bde2c9274f66f2bc53432da18e6
Parth <mittalparth22@gmail.com>
2022-05-31 01:06:01 +0530

    remove extra / from url

--- Inventory_Management/settings.py
+++ Inventory_Management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY_InvManage')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['inv-manage-college-clubs.herokuapp.com/', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['inv-manage-college-clubs.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
