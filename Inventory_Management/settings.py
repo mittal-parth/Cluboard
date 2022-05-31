ecd610d9431801821d2b1112b128ea7a0b13e8e5
Parth <mittalparth22@gmail.com>
2022-05-31 19:53:57 +0530

    add azure in  allowed hosts

--- Inventory_Management/settings.py
+++ Inventory_Management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY_InvManage')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['inv-manage-college-clubs.herokuapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['inv-manage-college-clubs.herokuapp.com', '127.0.0.1', 'localhost', 'invmanagecollegeclubsparth.azurewebsites.net/']
 
 
 # Application definition
